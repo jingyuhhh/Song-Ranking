0baaff9b5bf0341f8f2978de9ed8461818b77f49
fumingyang2004 <113994704+fumingyang2004@users.noreply.github.com>
2023-10-01 08:53:31 +0800

    keyword

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.data_analyse import data_analyse
 import pandas as pd
 import os
 import numpy as np
+from src.data_keyword import data_keyword
 
 if __name__ == '__main__':
     CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -12,4 +13,5 @@ if __name__ == '__main__':
         print('File not found')
         exit(1)
     data_analyse(data)
+    data_keyword(data)
 
