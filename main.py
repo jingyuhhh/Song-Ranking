d85f375a20a5ab21a78c46c739835102df8c0edf
JieleiZhang <113999701+JieleiZhang@users.noreply.github.com>
2023-10-02 10:53:17 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ if __name__ == '__main__':
     DATA_PATH = os.path.join(CURRENT_DIR, 'data/data.csv')
     try:
         data = pd.read_csv(DATA_PATH)
+        positive_word = pd.read_excel('data/positive_words.xlsx')
+        df_feature = pd.read_csv("data/featuresdf.csv")
     except FileNotFoundError:
         print('File not found')
         exit(1)
-    pre_process(data)
+    pre_process(data, positive_word, df_feature)
     data_analyse(data)
 
